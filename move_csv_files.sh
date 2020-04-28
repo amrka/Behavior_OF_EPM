7dd5eba6661255517f8270b401c92db31b13e01f
amrka <amr.kamel@uv.es>
2020-04-28 15:52:32 +0200

    adding the bash script to move files

---
+++ move_csv_files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Moving the metrics.csv files from the open_field_output directory of each subject
+
+cd /media/amr/Amr_4TB/Work/October_Acquistion/Open_Field_output
+
+cp  */*.csv .
+
+
+# combine all the csv files with a single header
+
+awk '(NR == 1) || (FNR > 1)' open_field_*.csv > open_field.csv
+
